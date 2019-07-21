5df8950b128b2f3991ac2b728768a595ee1eb3bc
Coricos <meryll.dindin@student.ecp.fr>
2019-07-21 04:05:01 -0700

    Correction for place description

--- interaction/application.py
+++ interaction/application.py
@@ -18,52 +18,52 @@ application = Flask(__name__)
 application.secret_key = crd['key']
 
 # Defines the request objects
-car = CarStatus()
-car.set_access(code=crd['token'])
-pos = car.get()
-
-# pos = {'latitude': 37.78742599487305, 
-#        'longitude': -122.39665222167969, 
-#        'range': 195.28, 
-#        'percentRemaining': 0.64}
 
 plc = PlacesOfInterest()
 jks = DadJoke()
 fct = FunFact()
+msg = ''
+pos = dict()
 
 @application.route('/exchange', methods=['POST'])
 def exchange():
 
     bdy, arg = '', dict(request.args)
-    # try: pos = car.get()
-    # except: pass
+    try: 
+        car = CarStatus()
+        car.set_access(code=crd['token'])
+        pos = car.get()
+    except:
+        pos = {'latitude': 37.78742599487305, 
+               'longitude': -122.39665222167969, 
+               'range': 195.28, 
+               'percentRemaining': 0.64}
 
     if arg['objective'] == 'enumerate':
         msg = plc.get(pos)
-        cor = ' ,'.join([e['title'] for e in msg])
+        cor = ', '.join([e['title'].strip() for e in msg])
         bdy = 'Do you wanna know more about your surroundings? ' 
-        bdy += 'Here are five places I picked for you: {}'.format(cor)
+        bdy += 'Here are five places I picked for you: {} '.format(cor)
+        bdy += 'Which one do you wanna hear about? Please give me a number.'
+
+    if arg['objective'] == 'describe':
+        try: bdy = wikipedia.summary(arg['title']).replace('\n',  ' ')
+        except: bdy = 'In fact I was kidding, I have no clue about what that place is all about...'
+
     if arg['objective'] == 'joke':
         bdy = 'Okay, here is a good one for you! '
         bdy += jks.get()
+
     if arg['objective'] == 'funfact':
         bdy = 'Have been scratching my head on this one. '
-        bdy += 'Did you know that ' + fct.get()
+        bdy += 'Did you know that ' + fct.get().lower()
 
-    if pos['percentRemaining'] < 0.70:
+    if pos['percentRemaining'] < 0.20:
         bdy += ' By the way, you should consider taking a small break, '
         bdy += 'and charge your car which is {} percent loaded.'.format(int(pos['percentRemaining']*100))
 
     arg = {'status': 200, 'mimetype': 'application/json'}
     return Response(response=json.dumps({'success': True, 'body': bdy}), **arg)
-
-@application.route('/refresh', methods=['POST'])
-def refresh():
-
-    car.set_access(code=None)
-
-    arg = {'status': 200, 'mimetype': 'application/json'}
-    return Response(response=json.dumps({'success': True}), **arg)
 
 # if __name__ == '__main__':
 
